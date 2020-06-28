7b6ec02309ff4dee1d415bcb866dd43482da09f2
ag2s20150909 <ag2s20150909@gmail.com>
2020-06-28 18:24:58 +0800

    Update GithubReleasesProxy.user.js

--- GithubReleasesProxy.user.js
+++ GithubReleasesProxy.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Github代理下载
-// @namespace    https://github.com/ag2s20150909/Mconfing/
+// @namespace    https://github.com/ag2s20150909/Mconfig/
 // @version      0.1
 // @description  利用国内的cdn加速下载github上的发布文件。
 // @author       Ag2S20150909
 // @match        https://github.com/*
-// @supportURL   https://github.com/ag2s20150909/Mconfing/issues
+// @supportURL   https://github.com/ag2s20150909/Mconfig/issues
 // @grant        none
 // ==/UserScript==
 
